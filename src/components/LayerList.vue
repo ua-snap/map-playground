ba0ad76fcbd66a803e5563467d594f7b9e5a6206
Charles Parr <cparr4@alaska.edu>
2021-07-29 13:25:31 -0800

    add pf layers that are in geoserver

--- src/components/LayerList.vue
+++ src/components/LayerList.vue
@@ -1,12 +1,52 @@
 <template>
   <div>
-    <h3 class="title is-4 top">Layers</h3>
+    <h3 class="title is-4 top">Mean Annual Ground Temperature</h3>
     <ul>
       <li>
-        <map-layer id="a42bed"></map-layer>
+        <map-layer id="1mgipl2050"></map-layer>
       </li>
       <li>
-        <map-layer id="alaska_landcover_2015"></map-layer>
+        <map-layer id="1mgipl2010"></map-layer>
+      </li>
+      <li>
+        <map-layer id="3mgipl2050"></map-layer>
+      </li>
+      <li>
+        <map-layer id="3mgipl2010"></map-layer>
+      </li>
+      <li>
+        <map-layer id="5mgipl2050"></map-layer>
+      </li>
+      <li>
+        <map-layer id="5mgipl2010"></map-layer>
+      </li>
+      <li>
+        <map-layer id="obumagt"></map-layer>
+      </li>
+    </ul>
+    <h3 class="title is-4 top">Active Layer Thickness</h3>
+    <ul>
+      <li>
+        <map-layer id="altGIPL2010"></map-layer>
+      </li>
+      <li>
+        <map-layer id="altGIPL2050"></map-layer>
+      </li>
+    </ul>
+    </ul>
+    <h3 class="title is-4 top">Permafrost Extent</h3>
+    <ul>
+      <li>
+        <map-layer id="pfextent_jorgenson"></map-layer>
+      </li>
+      <li>
+        <map-layer id="pfextent_obu"></map-layer>
+      </li>
+    </ul>
+    <h3 class="title is-4 top">Ground Ice Volume</h3>
+    <ul>
+      <li>
+        <map-layer id="icevol_jorgenson"></map-layer>
       </li>
     </ul>
   </div>
--- src/layers.js
+++ src/layers.js
@@ -1,17 +1,78 @@
 export default [
   {
-    // Internal app ID for layer, can be whatever.
-    id: "a42bed",
-    // Title of layer in list
-    title: "Layer Title",
-    // Full layer name in GeoServer, including namespace
-    wmsLayerName: "alaska_wildfires:postgis_lightning",
-    zindex: 20 // how this layer stacks relative to others
+    id: "1mgipl2050",
+    title: "GIPL Modeled Mean 2050 Ground Temperature at 1 m Depth",
+    wmsLayerName: "permafrost_beta:magt_1m_c_iem_gipl2_ar5_ncar_ccsm4_rcp85_2050_3338",
+    zindex: 3
   },
   {
-    id: "alaska_landcover_2015",
-    title: "Land cover types",
-    wmsLayerName: "alaska_wildfires:alaska_landcover_2015",
+    id: "1mgipl2010",
+    title: "GIPL Modeled Mean 2010 Ground Temperature at 1 m Depth",
+    wmsLayerName: "permafrost_beta:magt_1m_c_iem_gipl2_ar5_ncar_ccsm4_rcp85_2010_3338",
     zindex: 3
+  },
+  {
+    id: "3mgipl2050",
+    title: "GIPL Modeled Mean 2050 Ground Temperature at 3 m Depth",
+    wmsLayerName: "permafrost_beta:magt_3m_c_iem_gipl2_ar5_ncar_ccsm4_rcp85_2050_3338",
+    zindex: 3
+  },
+  {
+    id: "3mgipl2010",
+    title: "GIPL Modeled Mean 2010 Ground Temperature at 3 m Depth",
+    wmsLayerName: "permafrost_beta:magt_3m_c_iem_gipl2_ar5_ncar_ccsm4_rcp85_2010_3338",
+    zindex: 3
+  },
+  {
+    id: "5mgipl2050",
+    title: "GIPL Modeled Mean 2050 Ground Temperature at 5 m Depth",
+    wmsLayerName: "permafrost_beta:magt_5m_c_iem_gipl2_ar5_ncar_ccsm4_rcp85_2050_3338",
+    zindex: 3
+  },
+  {
+    id: "5mgipl2010",
+    title: "GIPL Modeled Mean 2010 Ground Temperature at 5 m Depth",
+    wmsLayerName: "permafrost_beta:magt_5m_c_iem_gipl2_ar5_ncar_ccsm4_rcp85_2010_3338",
+    zindex: 3
+  },
+  {
+    id: "obumagt",
+    title: "2000-2016 Modeled Mean Annual Ground Temperature at Top of Permafrost",
+    wmsLayerName: "obu_2018_magt",
+    zindex: 3
+  },
+  {
+  id: "altGIPL2010",
+  title: "GIPL Modeled 2010 Active Layer Thickness",
+  wmsLayerName: "permafrost_beta:alt_m_iem_gipl2_ar5_ncar_ccsm4_rcp85_2010_3338",
+  zindex: 3
+  },
+  {
+  id: "altGIPL2050",
+  title: "GIPL Modeled 2050 Active Layer Thickness",
+  wmsLayerName: "permafrost_beta:alt_m_iem_gipl2_ar5_ncar_ccsm4_rcp85_2050_3338",
+  zindex: 3
+  },
+  {
+  id: "pfextent_jorgenson",
+  title: "Permafrost Extent (Jorgenson et al., 2008)",
+  wmsLayerName: "permafrost_beta:jorgenson_2008_pf_extent_ground_ice_volume",
+  zindex: 3
+  },
+  {
+  id: "pfextent_obu",
+  title: "Permafrost Extent (Obu et al., 2018)",
+  wmsLayerName: "permafrost_beta:obu_pf_extent",
+  zindex: 3
+  },
+  {
+  id: "icevol_jorgenson",
+  title: "Ground Ice Volume (Jorgenson et al., 2008)",
+  wmsLayerName: "permafrost_beta:jorgenson_2008_pf_extent_ground_ice_volume",
+  styles: "ground_ice_volume",
+  zindex: 3
   }
+  //
+  // zindex: 3
+  // },
 ];
